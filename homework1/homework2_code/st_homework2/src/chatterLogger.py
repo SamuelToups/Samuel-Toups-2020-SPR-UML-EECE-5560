3c2d9e8d9a2e27e6ce4afa256c879f3cf2e3b3ff
Samuel Toups <samuel_toups@student.uml.edu>
2020-02-06 10:46:01 -0500

    Code for Homework 2

---
+++ homework1/homework2_code/st_homework2/src/chatterLogger.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+
+class chatterLogger:
+    def __init__(self):
+        rospy.Subscriber("chatter", String, self.callback)
+	self.pub = rospy.Publisher('changedChatter', String, queue_size=10)
+
+    def callback(self, data):
+        new_str = "MESSAGE:" + data.data
+        rospy.loginfo(new_str)
+        self.pub.publish(new_str)
+
+if __name__ == '__main__':
+    rospy.init_node('chatterLogger', anonymous=True)
+    chatterLogger()
+
+    rospy.spin()
