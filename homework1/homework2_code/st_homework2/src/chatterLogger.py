9f98d2f86cd9f866868fe9d66e8b5154ab25f9ce
Samuel Toups <samuel_toups@student.uml.edu>
2020-02-17 14:25:18 -0500

    fix problems from homework 2

--- homework1/homework2_code/st_homework2/src/chatterLogger.py
+++ homework1/homework2_code/st_homework2/src/chatterLogger.py
@@ -6,8 +6,8 @@ from std_msgs.msg import String
 class chatterLogger:
     def __init__(self):
         rospy.Subscriber("chatter", String, self.callback)
-	self.pub = rospy.Publisher('changedChatter', String, queue_size=10)
-
+        self.pub = rospy.Publisher('changedChatter', String, queue_size=10)
+    
     def callback(self, data):
         new_str = "MESSAGE:" + data.data
         rospy.loginfo(new_str)
@@ -16,5 +16,5 @@ class chatterLogger:
 if __name__ == '__main__':
     rospy.init_node('chatterLogger', anonymous=True)
     chatterLogger()
-
+    
     rospy.spin()
