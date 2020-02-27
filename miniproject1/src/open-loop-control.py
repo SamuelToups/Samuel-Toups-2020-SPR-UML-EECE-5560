aa3b471dfb4c7b750e4198c555050ae5fd8b3809
SamuelToups <samuel_toups@student.uml.edu>
2020-02-27 14:26:35 +0000

    more work on miniproject1

--- miniproject1/src/open-loop-control.py
+++ miniproject1/src/open-loop-control.py
@@ -44,17 +44,64 @@ if __name__ == "__main__":
         cmd4.header.stamp = rospy.get_rostime()
         cmd4.header.seq = 4
         cmd4.v = 0.0
-        cmd4.omega = 0.0
+        cmd4.omega = -8.0
+        
+        cmd5 = Twist2DStamped()
+        cmd5.header = Header()
+        cmd5.header.stamp = rospy.get_rostime()
+        cmd5.header.seq = 1
+        cmd5.v = 0.5
+        cmd5.omega = 0.0
+        
+        cmd6 = Twist2DStamped()
+        cmd6.header = Header()
+        cmd6.header.stamp = rospy.get_rostime()
+        cmd6.header.seq = 1
+        cmd6.v = -0.5
+        cmd6.omega = 0.0
+        
+        cmd7 = Twist2DStamped()
+        cmd7.header = Header()
+        cmd7.header.stamp = rospy.get_rostime()
+        cmd7.header.seq = 1
+        cmd7.v = 0.0
+        cmd7.omega = 8.0
+        
+        cmd8 = Twist2DStamped()
+        cmd8.header = Header()
+        cmd8.header.stamp = rospy.get_rostime()
+        cmd8.header.seq = 1
+        cmd8.v = 0.5
+        cmd8.omega = 0.0
+        
+        cmd9 = Twist2DStamped()
+        cmd9.header = Header()
+        cmd9.header.stamp = rospy.get_rostime()
+        cmd9.header.seq = 1
+        cmd9.v = 0.0
+        cmd9.omega = 0.0
         
         while not rospy.is_shutdown():
             rospy.sleep(0.1)
             opc.cmd_pub.publish(cmd1)
-            rospy.sleep(0.2)
+            rospy.sleep(0.5)
             opc.cmd_pub.publish(cmd2)
-            rospy.sleep(0.202)
-            opc.cmd_pub.publish(cmd3)
             rospy.sleep(0.2)
+            opc.cmd_pub.publish(cmd3)
+            rospy.sleep(0.5)
             opc.cmd_pub.publish(cmd4)
-            rospy.sleep(1.)
+            rospy.sleep(0.2)
+            opc.cmd_pub.publish(cmd5)
+            rospy.sleep(0.5)
+            opc.cmd_pub.publish(cmd9)
+            rospy.sleep(0.1)
+            opc.cmd_pub.publish(cmd6)
+            rospy.sleep(0.5)
+            opc.cmd_pub.publish(cmd7)
+            rospy.sleep(0.2)
+            opc.cmd_pub.publish(cmd8)
+            rospy.sleep(0.5)
+            opc.cmd_pub.publish(cmd9)
+            rospy.sleep(5.0)
     except rospy.ROSInterruptException:
         pass
